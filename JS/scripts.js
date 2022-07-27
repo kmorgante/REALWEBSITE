789098fdf70b39fbe1444b912f4f06eee8748e6c
kmorgante <106821307+kmorgante@users.noreply.github.com>
2022-07-27 10:05:08 -0400

    Finished JavaScript and fixed hamburger

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,7 +2,7 @@ console.log("Hey! I'm Kelsey. Welcome to my portfolio site. I hope you enjoy!");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
